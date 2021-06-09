91a9f4922cafcc2844558c7afbd5110695cae343
parag dangui <developer@kodework.com>
2021-06-09 14:17:01 +0530

    the content is added to the front page + classic editor is added

--- wp-content/themes/theme-name/front-page.php
+++ wp-content/themes/theme-name/front-page.php
@@ -1,4 +1,4 @@
-<?php get_header();?>
+<?php get_header(); ?>
 
 
 
@@ -7,15 +7,19 @@
 
 <div class="container">
   <div class="row">
-      <div class="col">
-          <p> this is row one </p>
-      </div>
-
-      <div class="col">
-        <p> this is row two </p>
+    <div class="col">
+      <p> this is row one </p>
     </div>
+
+    <div class="col">
+      <p> this is row two </p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
+      <?php the_content(); ?></div>
   </div>
 </div>
 
 
-<?php get_footer();?>
+<?php get_footer(); ?>
